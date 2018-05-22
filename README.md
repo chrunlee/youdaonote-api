4bf048ef85bc524fc28afbb788094206f132387e
chrunlee <chrunlee@foxmail.com>
2018-05-22 18:01:12 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,140 @@
 # youdaonote-api
 有道云笔记-Nodejs 版本API 
+
+# 概述
+使用oauth2.0,访问有道云笔记API，对笔记、笔记本、资源进行操作。
+
+# 参考
+[有道云API](http://note.youdao.com/open/apidoc.html)
+
+# 安装
+首先在[http://note.youdao.com/open/developguide.html#app](http://note.youdao.com/open/developguide.html#app)申请自己的ConsumerKey，之后才可以使用。
+
+然后安装模块`npm install ydnote`即可。
+
+# 使用
+
+```
+var YNote = require('ydnote');
+
+var youdao = new YNote({
+	clientId : '',//申请的customerkey
+	clientSecret : '',//申请的密钥
+	oauthUrl : ''//申请的授权地址
+});
+
+//获得拼装后的授权地址，进行跳转
+var oauthUrl = youdao.getOAuthUrl();
+
+//用户授权后返回到授权地址，然后进行其他操作，注意：获得token后需要保存起来，进行判断，如果没有token或过期，需要重新获取(目前还没优化)
+youdao.getToken(code)
+.then(function(token){
+	console.log(token);//获得token信息
+})
+.catch(function(e){
+	console.log(e);//报错
+})
+
+//获得用户信息
+
+youdao.getUserInfo(token).then(function(userInfo){
+	console.log(userInfo);
+})
+
+// 获得所有的笔记本信息
+youdao.getAllNotebook(token).then(function( bookList ){
+ 	console.log(`笔记本个数有：${bookList.length}`);
+}).catch(function(e){
+ 	console.log(e.status);
+ 	console.log(e.message);
+ 	console.log(e.response.text)
+})
+
+
+//获得笔记本内的所有笔记信息
+youdao.getNoteOfBook(token,'/513BEFD144B84F28923AE83B09BB3DF2').then(function( noteList ){
+	console.log(`笔记个数有：${noteList.length}`);
+}).catch(function(e){
+	console.log(e.status);
+	console.log(e.message);
+})
+
+//创建文件夹和删除
+youdao.createNotebook(token,'API创建笔记本').then(function(rsObj){
+	console.log('笔记本创建成功:'+rsObj.path);
+	return youdao.deleteNotebook(token,rsObj.path);
+}).then(function(){
+	console.log('笔记本删除成功')
+}).catch(function(e){
+	console.log(e.status);
+	console.log(e.message);
+	console.log(e);
+})
+
+
+//创建笔记
+youdao.createNote(token,{
+	content : '<html><body><p style="color:red">笔记创建测试</p></body></html>',
+	// content : 'aaa',
+	title : '测试'
+}).then(function(rsObj){
+	console.log(rsObj);
+//获得笔记信息
+	return youdao.getNote(token,rsObj.path);
+})
+.then(function(rsObj){
+	console.log(rsObj);
+//更新笔记
+	return youdao.updateNote(token,{
+		path : rsObj.path,
+		content : '修改'
+	})
+})
+.then(function(rsObj){
+	console.log(rsObj);
+//移动笔记
+	return youdao.moveNote(token,rsObj.path,'/513BEFD144B84F28923AE83B09BB3DF2')
+})
+.then(function(rsObj){
+	console.log(rsObj);
+//删除笔记
+	return youdao.deleteNote(token,rsObj.path)
+})
+.then(function(rsObj){
+	console.log(rsObj);
+//分享笔记
+	return youdao.publishNote(token,rsObj.path);
+})
+.then(function(rsObj){
+	console.log(rsObj);
+})
+.catch(function(e){
+	console.log(e.status);
+	console.log(e.message);
+	console.log(e.response.text);
+})
+
+//文件上传
+youdao.upload(token,__dirname+'/test.js')
+.then(function(rsObj){
+	console.log(rsObj);
+})
+.catch(function(e){
+	console.log(e);
+})
+
+//文件下载
+youdao.download(token,'https://note.youdao.com/yws/open/resource/download/20114/140BC634A1D9455DAC1E0BFBBD7894C5','/home/test.js')
+.then(function(rs){
+	console.log(rs);
+})
+.catch(function(e){
+	console.log(e);
+})
+```
+
+# 说明
+目前是第一版，还没有进行测试和优化，应该会有好多问题，欢迎各位拍砖。
+
+#LICENSE 
+MIT
---
+++ YNote.js
@@ -0,0 +1,297 @@
+//--------
+// 有道云笔记 Nodejs sdk
+// @created by chrunlee
+// @since 2018-05-21
+//--------
+
+//依赖模块
+var qs = require('querystring'),
+	request = require('superagent'),
+	fs = require('fs');
+
+// 
+// 创建有道云SDK实例
+// opts = {
+// 	clientId : '',//申请的id
+// 	clientSecret : '',//申请的密钥
+//  oauthUrl : '',//申请的授权后回调地址
+//	accessUrl : '',//申请的token回调地址
+// }
+//
+function YNote( opts ){
+	var _default = {
+		state : 'state',
+		display : 'web',
+		responseType : 'code',
+		grantType : 'authorization_code',
+		//以下为有道笔记的api 地址，建议不要覆盖
+		//有道笔记的oauth 授权地址
+		_oauth_url : 'https://note.youdao.com/oauth/authorize2',
+		//有道笔记的access token 获取地址
+		_access_url : 'https://note.youdao.com/oauth/access2',
+		//有道笔记的user api地址：获得用户信息
+		_user_url : 'https://note.youdao.com/yws/open/user/get.json',
+		//笔记本
+		_notebook : {
+			//有道笔记的notebook api 地址：获得所有的笔记本
+			_list_url : 'https://note.youdao.com/yws/open/notebook/all.json',
+			//有道笔记的note api 地址：获取笔记本下的所有笔记
+			_note_url : 'https://note.youdao.com/yws/open/notebook/list.json',
+			//有道笔记的create api 地址：创建笔记本
+			_create_url : 'https://note.youdao.com/yws/open/notebook/create.json',
+			//有道笔记的delete api地址：删除笔记
+			_delete_url : 'https://note.youdao.com/yws/open/notebook/delete.json'
+		},
+		//笔记
+		_note : {
+			//有道笔记的create api 地址：创建笔记
+			_create_url : 'https://note.youdao.com/yws/open/note/create.json',
+			//有道笔记的get api 地址：查看笔记信息
+			_get_url : 'https://note.youdao.com/yws/open/note/get.json',
+			//有道笔记API ： 修改笔记内容
+			_update_url : 'https://note.youdao.com/yws/open/note/update.json',
+			//有道云笔记API ： 移动笔记到目标笔记本
+			_move_url : 'https://note.youdao.com/yws/open/note/move.json',
+			//有道云笔记API ：删除笔记
+			_delete_url : 'https://note.youdao.com/yws/open/note/delete.json',
+			//有道云笔记API：发布笔记/分享
+			_publish_url :'https://note.youdao.com/yws/open/share/publish.json',
+			//有道云笔记API：上传文件
+			_upload_url : 'https://note.youdao.com/yws/open/resource/upload.json'
+		}
+	};
+	this.config = Object.assign(_default,opts);
+	// if(!this.config.clientId || !this.config.clientSecret || !this.config.oauthUrl){
+	// 	throw new Error('缺少配置项:clientId/clientSecret/oauthUrl');
+	// }
+	return this;
+}
+
+
+YNote.prototype = {
+	//返回数据
+	getPromiseGET : function( url,exefn){
+		return new Promise(function(resolve,reject){
+			request.get(url)
+			.then(function(rs){
+				var rsObj = !rs || rs.text === '' ? {} : JSON.parse(rs.text);
+				if(exefn)exefn(rsObj);
+				resolve(rsObj);
+			})
+			.catch(function(e){
+				reject(e);
+			});
+		});
+	}
+	,getPromisePOST : function(url,exefn){
+		return new Promise(function(resolve,reject){
+			request.post(url)
+			.then(function(rs){
+				var rsObj = !rs || rs.text === '' ? {} : JSON.parse(rs.text);
+				if(exefn)exefn(rsObj);
+				resolve(rsObj);
+			})
+			.catch(function(e){
+				reject(e);
+			});
+		});
+	}
+	,getPromiseJSON : function(url,data,exefn){
+		return new Promise(function(resolve,reject){
+			request.post(url)
+			.set('Content-type','application/x-www-form-urlencoded')
+			.send(data)
+			.then(function(rs){
+				var rsObj = !rs || rs.text === '' ? {} : JSON.parse(rs.text);
+				if(exefn)exefn(rsObj);
+				resolve(rsObj);
+			})
+			.catch(function(e){
+				reject(e);
+			});
+		});
+	}
+	,getPromiseMULTI : function(url,token,data,exefn){
+		return new Promise(function(resolve,reject){
+			request.post(url)
+			.set('Content-Type','multipart/form-data')
+			.set('Authorization','OAuth oauth_token="'+token+'"')
+			.field(data)
+			.then(function(rs){
+				var rsObj = !rs || rs.text === '' ? {} : JSON.parse(rs.text);
+				if(exefn)exefn(rsObj);
+				resolve(rsObj);
+			})
+			.catch(function(e){
+				reject(e);
+			});
+		});
+	}
+	,getPromiseFILE : function(url,token,filePath,exefn){
+		return new Promise(function(resolve,reject){
+			request.post(url)
+			.set('Content-Type','multipart/form-data')
+			.set('Authorization','OAuth oauth_token="'+token+'"')
+			// .field(data)
+			.attach('file',filePath)
+			.then(function(rs){
+				var rsObj = !rs || rs.text === '' ? {} : JSON.parse(rs.text);
+				if(exefn)exefn(rsObj);
+				resolve(rsObj);
+			})
+			.catch(function(e){
+				reject(e);
+			});
+		});
+	}
+	//获得有道笔记的授权登录地址
+	,getOAuthUrl : function(){
+		var config = this.config;
+		return config._oauth_url+'?client_id='+config.clientId+'&response_type='+config.responseType+'&redirect_uri='+config.oauthUrl+'&state='+config.state+'&display='+config.display;
+	}
+	//获得有道笔记请求access_token的地址
+	,getAccessUrl : function( code ){
+		var config = this.config;
+		config._code = code;
+		return config._access_url+'?client_id='+config.clientId+'&client_secret='+config.clientSecret+'&redirect_uri='+config.oauthUrl+'&grant_type='+config.grantType+'&code='+code;
+	}
+	//根据request 获得token
+	,getToken : function( code ){
+		var that = this,config = that.config;
+		if(config._access_token){
+			return config._access_token;
+		}
+		if(!code && !config._code ){
+			throw new Error('缺少配置项 code');
+		}
+		var accessUrl = that.getAccessUrl(code);
+		return that.getPromiseGET(accessUrl,function(rsObj){
+			if(rsObj.accessToken){
+				config._access_token = rsObj.accessToken;
+			}
+		})
+	}
+	//获得用户信息
+	,getUserInfo : function( token ){
+		var that = this,config = this.config;
+		return that.getPromiseGET(config._user_url+'?oauth_token='+token || config._access_token);
+	}
+	//获得
+	,getAllNotebook : function( token ){
+		var that = this,config = that.config;
+		return that.getPromisePOST(config._notebook._list_url+'?oauth_token='+token || config._access_token)
+	}
+	//获得笔记本里的笔记所有笔记信息
+	,getNoteOfBook(token,bookpath){
+		var that = this,config = that.config;
+		return that.getPromiseJSON(config._notebook._note_url,{
+			'notebook':bookpath||'',
+			'oauth_token':token || config._access_token
+		});
+	}
+	//创建笔记本
+	,createNotebook : function( token,name,createtime){
+		var that = this,config = that.config;
+		return that.getPromiseJSON(config._notebook._create_url,{
+			'oauth_token' : token || config._access_token,
+			'name' : name || '新建文件夹',
+			'createtime' : createtime || ''
+		});
+	}
+	//删除笔记本
+	,deleteNotebook : function( token, notebookpath,modifyTime ){
+		var that = this,config = that.config;
+		return that.getPromiseJSON(config._notebook._delete_url,{
+			'oauth_token' : token || config._access_token,
+			'notebook' : notebookpath || '',
+			'modify_time' : modifyTime || ''
+		});
+	}
+	//创建笔记
+	//note = {
+	//  title : '',//string
+	//	content : '',//string
+	// 	author : '',//string
+	//	source : '',//string
+	// 	create_time : '',//number 单位为秒
+	//	notebook : ''//string
+	//}
+	,createNote : function( token ,note ){
+		var that = this,config = that.config;
+		note = Object.assign({
+			'oauth_token' : token ||config._access_token
+		},note);
+		if(!note.content){
+			throw new Error('创建笔记缺少content内容');
+		}
+		return that.getPromiseMULTI(config._note._create_url,note.oauth_token,note)
+	}
+	//根据notepath 获得笔记的详细信息
+	,getNote : function( token ,notepath ){
+		var that = this,config = that.config;
+		if(!notepath){
+			throw new Error('笔记缺少路径');
+		}
+		return that.getPromiseJSON(config._note._get_url,{
+			'oauth_token' : token || config._access_token,
+			'path' : notepath
+		});
+	}
+	//根据notepath 修改笔记内容
+	,updateNote : function( token ,note ){
+		var that = this,config = that.config;
+		if(!note || !note.path || !note.content){
+			throw new Error('更新笔记缺少path 或content数据')
+		}
+		return that.getPromiseMULTI(config._note._update_url,token || config._access_token,note);
+	}
+	//移动笔记到目标笔记本
+	,moveNote : function( token,path,notebook){
+		var that = this,config = that.config;
+		return that.getPromiseJSON(config._note._move_url,{
+			oauth_token : token || config._access_token,
+			path : path,
+			notebook : notebook
+		});
+	}
+	//删除笔记
+	,deleteNote : function(token,notepath){
+		var that = this,config = that.config;
+		return that.getPromiseJSON(config._note._delete_url,{
+			oauth_token : token || config._access_token,
+			path : notepath
+		});
+	}
+	//分享笔记
+	,publishNote : function(token,notepath){
+		var that = this,config = that.config;
+		return that.getPromiseJSON(config._note._publish_url,{
+			oauth_token : token || config._access_token,
+			path : notepath
+		})
+	}
+	//上传
+	,upload : function(token,filePath){
+		var that= this,config= that.config;
+		return that.getPromiseFILE(config._note._upload_url,token||config._access_token,filePath);
+	}
+	,download : function(token,url,filePath){
+		var that = this,config = that.config;
+		return new Promise(function(resolve,reject){
+			request.get(url+'?oauth_token='+token)
+			.then(function(res){
+				fs.writeFile(filePath,res.body,function(){
+					resolve(filePath);
+				})
+			})
+			.catch(function(e){
+				reject(e);
+			})
+		});
+		
+
+	}
+
+};
+
+module.exports = YNote;
